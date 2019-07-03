d1fd215ad104dbc9200adea5d0adb136e73f4e36
QRAVET <quentin.ravet@yahoo.fr>
2019-07-03 22:19:26 +0200

    idem bis

--- appVAB/functions/db_connect.php
+++ appVAB/functions/db_connect.php
@@ -1,10 +1,10 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 
-$db_host = "localhost";
-$db_name = "partiel3";
-$db_user = "root";
-$db_password = "";
+$db_host = "db5000114394.hosting-data.io";
+$db_name = "dbs108962";
+$db_user = "dbu184636";
+$db_password = "Lecielestbleu!02";
 
 // Connect to database
 $database = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
